c7dcc3ff372647a7201da93ee1936cd855ecf0e2
NhicolasAcosta <aco21007@byui.edu>
2022-01-26 02:26:00 -0300

    Lesson-3-completed

--- lesson3/js/script.js
+++
@@ -1,7 +0,0 @@
-function teste(){
-    document.querySelector("#date").innerHTML = "Last Updated: " + document.lastModified;
-    document.querySelector("#copy").innerHTML = "&copy;"
-    document.querySelector('#year').innerHTML = new Date(Date.now()).getFullYear();
-  }
-  
-  
