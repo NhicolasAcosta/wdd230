6cb1161a47a24be3955638ad037db3a72401e5c9
NhicolasAcosta <aco21007@byui.edu>
2022-10-15 12:48:22 -0300

    BOM

---
+++ bom/main.js
@@ -0,0 +1,35 @@
+const addList = document.querySelector("ul");
+const addInput = document.querySelector("input");
+const addButton = document.querySelector("button");
+
+addButton.addEventListener("click", () => {
+    let book = addInput.value;
+    addInput.value = "";
+
+    let listBook = document.createElement("li");
+    let listName = document.createElement("span");
+    let listButton = document.createElement("button");
+ 
+    listBook.appendChild(listName);
+    listBook.appendChild(listButton);
+    listName.textContent = book;
+    listButton.textContent = "X";
+    addList.appendChild(listBook);
+
+    
+    
+
+    listButton.addEventListener("click", () => {
+        addList.removeChild(listBook);
+    })
+    addList.appendChild(listBook);
+
+
+    
+    
+
+    
+
+})
+
+ 
