c38d29f3d72daa60379acf709d69e08f1a59b26b
AcostaNhicolas <nhicolasacostapro@gmail.com>
2022-11-23 01:00:06 -0300

    Weather Chamber

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,50 @@
-let t = parseFloat(document.querySelector('#temperature').textContent)
-let s = parseFloat(document.querySelector('#windSpeed').textContent)
- 
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const currentTemp = document.querySelector('#current-temp');
+const wSpeed = document.querySelector('#wSpeed');
+const wChill = document.querySelector('#wChill');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=81887b7776f7c49434d1ab8d31032037';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        array = displayResults(data);
+        elements(array)
+
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+apiFetch();
+
+
+function  displayResults(weatherData) {
+    const temperature = weatherData.main.temp;
+    const windSpeed = weatherData.wind.speed; 
+    const windChill = windChillCalculate(temperature, windSpeed)
+    const description = weatherData.weather[0].description;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    return [temperature, windSpeed, windChill, description, iconsrc]
+}
+
+function elements(array) {
+    currentTemp.textContent = array[0]
+    wSpeed.textContent = array[1]
+    wChill.textContent = array[2]
+    captionDesc.textContent = array[3]
+    weatherIcon.setAttribute('src', array[4])
+    weatherIcon.setAttribute('alt', array[3])
+}
+
+
+
 function windChillCalculate(t, s){
     if (t<=50){
         if (s > 3.0){
@@ -15,4 +59,3 @@ function windChillCalculate(t, s){
         return wc
     }
 }
-let windChill = document.getElementById('windChill').innerHTML = windChillCalculate(t,s);
