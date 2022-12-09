27fe6816b115ef16131a787c528d000e47149c32
NhicolasAcosta <aco21007@byui.edu>
2022-12-09 14:05:55 -0300

    home-09-12

--- Final Proyect/js/scripts.js
+++ Final Proyect/js/scripts.js
@@ -1,3 +1,5 @@
+// ---------- HAMBURGER MENU ----------
+
 function toggleMenu() {
     document.getElementById('navegation').classList.toggle('open');
     document.getElementById('hamburgerBtn').classList.toggle('open');
@@ -5,3 +7,34 @@ function toggleMenu() {
 
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
+
+// ---------- PICTURE ----------
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshold: 0.85,
+    rootMargin: '0px'
+};
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {image.removeAttribute('data-src');};
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+      imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+}
